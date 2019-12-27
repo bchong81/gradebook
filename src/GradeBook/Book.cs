09bf19e12e173144c53d4a6d6329262b2e04ede5
Brian Chong <bchong81@gmail.com>
2019-12-26 21:13:49 -0500

    Initial commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.Name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<double> grades;
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+            
+            return result;
+        }
+
+        public string Name;
+    }
+}
