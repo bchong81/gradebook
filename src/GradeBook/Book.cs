506bdc0b69a44d7f6dfb2a494970316aedf258f4
Brian Chong <bchong81@gmail.com>
2019-12-27 23:11:38 -0500

    User input added

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -10,9 +10,31 @@ namespace GradeBook
             grades = new List<double>();
             this.Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
         }
 
         private List<double> grades;
@@ -24,14 +46,43 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            for (var index = 0; index < grades.Count; index++)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                if (grades[index] == 42.1)
+                {
+                    goto done;
+                }
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
             }
+
             result.Average /= grades.Count;
-            
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                result.Letter = 'A';
+                break;
+
+                case var d when d >= 80.0:
+                result.Letter = 'B';
+                break;
+
+                case var d when d >= 70.0:
+                result.Letter = 'C';
+                break;
+
+                case var d when d >= 60.0:
+                result.Letter = 'D';
+                break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
+        done:
             return result;
         }
 
